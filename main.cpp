c69b66d93f5944a3e97c992dcfe4f62ebee9d41f
anubis <anub6534@gmail.com>
2026-03-08 12:22:08 +0900

    ウィンドウの解像度、サイズ、タイトル、背景色、垂直同期、ゲームの中断設定、アイコンについてと、マジックナンバーのマクロ定義について学んだ

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "DxLib.h"
+
+//マクロ定義　//マジックナンバー(急に出てくる数字)を定義する
+#define GameWidth 1280				// ゲームの横幅
+#define GameHeight 720				// ゲームの縦幅  //ctrl + dで同じ行を複製する
+#define GameColor   32				// ゲームの色
+#define GameTitle "Title"			//画面タイトル
+#define GameVSync TRUE				//V-Syncを使うならTRUE
+#define GameIcon   555				//画面アイコン
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	SetOutApplicationLogValidFlag(FALSE);			//Log.txtを作成しないように設定
+	ChangeWindowMode(TRUE);							//ウィンドウモードに設定 
+	SetGraphMode(GameWidth, GameHeight, GameColor); //解像度を1280x720、色数を32ビットに設定
+	SetWindowSize(GameWidth, GameHeight);			//ウィンドウサイズを1280x720に設定
+	SetMainWindowText(GameTitle);					//ウィンドウタイトルを設定
+	SetBackgroundColor(0, 0, 0);					//背景色を黒に設定
+	SetWaitVSyncFlag(GameVSync);					//垂直同期を設定
+	SetAlwaysRunFlag(TRUE);							//ウィンドウがアクティブでないときも処理を続行する);
+	SetWindowIconID(GameIcon);						//アイコン					
+
+	// Initialize the library
+	if (DxLib_Init() == -1) {
+		return -1; // Initialization failed
+	}
+	
+	//文字を画面に出力
+	DrawString(0, 0, "Hello, DxLib!", GetColor(255, 255, 255));
+	WaitKey(); // Wait for a key press(キーが入力されると画面を閉じる)
+	DxLib_End(); // Clean up and exit
+	return 0;  //ソフト終了
+}
